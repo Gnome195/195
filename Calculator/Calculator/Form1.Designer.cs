53dc8239ce6cfd50c58b1dcad4fceb8445510471
mohammad sadegh bakhshi <sadeghxt@gmail.com>
2025-04-06 20:47:04 +0330

    Add files via upload

---
+++ Calculator/Calculator/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.sum = new System.Windows.Forms.Button();
+            this.mines = new System.Windows.Forms.Button();
+            this.multy = new System.Windows.Forms.Button();
+            this.lnlog = new System.Windows.Forms.Button();
+            this.logarithm = new System.Windows.Forms.Button();
+            this.Division = new System.Windows.Forms.Button();
+            this.factorial = new System.Windows.Forms.Button();
+            this.power = new System.Windows.Forms.Button();
+            this.power2 = new System.Windows.Forms.Button();
+            this.radical = new System.Windows.Forms.Button();
+            this.modulus = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // sum
+            // 
+            this.sum.BackColor = System.Drawing.Color.Silver;
+            this.sum.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.sum.Location = new System.Drawing.Point(12, 130);
+            this.sum.Name = "sum";
+            this.sum.Size = new System.Drawing.Size(90, 32);
+            this.sum.TabIndex = 2;
+            this.sum.Text = "+";
+            this.sum.UseVisualStyleBackColor = false;
+            this.sum.Click += new System.EventHandler(this.sum_Click);
+            // 
+            // mines
+            // 
+            this.mines.BackColor = System.Drawing.Color.Silver;
+            this.mines.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.mines.Location = new System.Drawing.Point(144, 130);
+            this.mines.Name = "mines";
+            this.mines.Size = new System.Drawing.Size(90, 32);
+            this.mines.TabIndex = 3;
+            this.mines.Text = "-";
+            this.mines.UseVisualStyleBackColor = false;
+            this.mines.Click += new System.EventHandler(this.mines_Click);
+            // 
+            // multy
+            // 
+            this.multy.BackColor = System.Drawing.Color.Silver;
+            this.multy.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.multy.Location = new System.Drawing.Point(274, 130);
+            this.multy.Name = "multy";
+            this.multy.Size = new System.Drawing.Size(90, 32);
+            this.multy.TabIndex = 4;
+            this.multy.Text = "*";
+            this.multy.UseVisualStyleBackColor = false;
+            this.multy.Click += new System.EventHandler(this.multy_Click);
+            // 
+            // lnlog
+            // 
+            this.lnlog.BackColor = System.Drawing.Color.Silver;
+            this.lnlog.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lnlog.Location = new System.Drawing.Point(274, 183);
+            this.lnlog.Name = "lnlog";
+            this.lnlog.Size = new System.Drawing.Size(90, 32);
+            this.lnlog.TabIndex = 7;
+            this.lnlog.Text = "na Log";
+            this.lnlog.UseVisualStyleBackColor = false;
+            this.lnlog.Click += new System.EventHandler(this.lnlog_Click);
+            // 
+            // logarithm
+            // 
+            this.logarithm.BackColor = System.Drawing.Color.Silver;
+            this.logarithm.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.logarithm.Location = new System.Drawing.Point(144, 183);
+            this.logarithm.Name = "logarithm";
+            this.logarithm.Size = new System.Drawing.Size(90, 32);
+            this.logarithm.TabIndex = 6;
+            this.logarithm.Text = "Logarithm";
+            this.logarithm.UseVisualStyleBackColor = false;
+            this.logarithm.Click += new System.EventHandler(this.logarithm_Click);
+            // 
+            // Division
+            // 
+            this.Division.BackColor = System.Drawing.Color.Silver;
+            this.Division.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Division.Location = new System.Drawing.Point(12, 183);
+            this.Division.Name = "Division";
+            this.Division.Size = new System.Drawing.Size(90, 32);
+            this.Division.TabIndex = 5;
+            this.Division.Text = "/";
+            this.Division.UseVisualStyleBackColor = false;
+            this.Division.Click += new System.EventHandler(this.Division_Click);
+            // 
+            // factorial
+            // 
+            this.factorial.BackColor = System.Drawing.Color.Silver;
+            this.factorial.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.factorial.Location = new System.Drawing.Point(274, 234);
+            this.factorial.Name = "factorial";
+            this.factorial.Size = new System.Drawing.Size(90, 32);
+            this.factorial.TabIndex = 10;
+            this.factorial.Text = "Factorial";
+            this.factorial.UseVisualStyleBackColor = false;
+            this.factorial.Click += new System.EventHandler(this.factorial_Click);
+            // 
+            // power
+            // 
+            this.power.BackColor = System.Drawing.Color.Silver;
+            this.power.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.power.Location = new System.Drawing.Point(144, 234);
+            this.power.Name = "power";
+            this.power.Size = new System.Drawing.Size(90, 32);
+            this.power.TabIndex = 9;
+            this.power.Text = "power";
+            this.power.UseVisualStyleBackColor = false;
+            this.power.Click += new System.EventHandler(this.power_Click);
+            // 
+            // power2
+            // 
+            this.power2.BackColor = System.Drawing.Color.Silver;
+            this.power2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.power2.Location = new System.Drawing.Point(12, 234);
+            this.power2.Name = "power2";
+            this.power2.Size = new System.Drawing.Size(90, 32);
+            this.power2.TabIndex = 8;
+            this.power2.Text = "Power 2";
+            this.power2.UseVisualStyleBackColor = false;
+            this.power2.Click += new System.EventHandler(this.power2_Click);
+            // 
+            // radical
+            // 
+            this.radical.BackColor = System.Drawing.Color.Silver;
+            this.radical.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.radical.Location = new System.Drawing.Point(207, 296);
+            this.radical.Name = "radical";
+            this.radical.Size = new System.Drawing.Size(90, 32);
+            this.radical.TabIndex = 12;
+            this.radical.Text = "Radical";
+            this.radical.UseVisualStyleBackColor = false;
+            this.radical.Click += new System.EventHandler(this.radical_Click);
+            // 
+            // modulus
+            // 
+            this.modulus.BackColor = System.Drawing.Color.Silver;
+            this.modulus.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.modulus.Location = new System.Drawing.Point(75, 296);
+            this.modulus.Name = "modulus";
+            this.modulus.Size = new System.Drawing.Size(90, 32);
+            this.modulus.TabIndex = 11;
+            this.modulus.Text = "modulus";
+            this.modulus.UseVisualStyleBackColor = false;
+            this.modulus.Click += new System.EventHandler(this.modulus_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.GrayText;
+            this.textBox1.ForeColor = System.Drawing.SystemColors.Window;
+            this.textBox1.Location = new System.Drawing.Point(75, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(285, 22);
+            this.textBox1.TabIndex = 13;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.GrayText;
+            this.textBox2.ForeColor = System.Drawing.SystemColors.Window;
+            this.textBox2.Location = new System.Drawing.Point(75, 85);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(285, 22);
+            this.textBox2.TabIndex = 14;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 16);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "num 1 :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 16);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "num 2 :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(387, 358);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.radical);
+            this.Controls.Add(this.modulus);
+            this.Controls.Add(this.factorial);
+            this.Controls.Add(this.power);
+            this.Controls.Add(this.power2);
+            this.Controls.Add(this.lnlog);
+            this.Controls.Add(this.logarithm);
+            this.Controls.Add(this.Division);
+            this.Controls.Add(this.multy);
+            this.Controls.Add(this.mines);
+            this.Controls.Add(this.sum);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "my Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button sum;
+        private System.Windows.Forms.Button mines;
+        private System.Windows.Forms.Button multy;
+        private System.Windows.Forms.Button lnlog;
+        private System.Windows.Forms.Button logarithm;
+        private System.Windows.Forms.Button Division;
+        private System.Windows.Forms.Button factorial;
+        private System.Windows.Forms.Button power;
+        private System.Windows.Forms.Button power2;
+        private System.Windows.Forms.Button radical;
+        private System.Windows.Forms.Button modulus;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Calculator/Calculator/Form1.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void sum_Click(object sender, EventArgs e)
+        {
+            
+            if (textBox1.Text!=""&&textBox2.Text!="")
+            {
+                double numsum1 = Convert.ToDouble(textBox1.Text);
+                double numsum2 = Convert.ToDouble(textBox2.Text);
+                double sum = numsum1 + numsum2;
+                MessageBox.Show("resul is = " + sum);
+            }
+            else
+            {
+                MessageBox.Show("Please enter the required values !");
+            }
+           
+        }
+
+        private void mines_Click(object sender, EventArgs e)
+        {
+           
+            if (textBox1.Text != "" && textBox2.Text != "")
+            {
+                double nummines1 = Convert.ToDouble(textBox1.Text);
+                double nummines2 = Convert.ToDouble(textBox2.Text);
+                double mines = nummines1 - nummines2;
+                MessageBox.Show("resul is = " + mines);
+            }
+            else
+            {
+                MessageBox.Show("Please enter the required values !");
+            }
+        }
+
+        private void multy_Click(object sender, EventArgs e)
+        {
+           
+            if (textBox1.Text != "" && textBox2.Text != "")
+            {
+                double nummulty1 = Convert.ToDouble(textBox1.Text);
+                double nummulty2 = Convert.ToDouble(textBox2.Text);
+                double multy = nummulty1 * nummulty2;
+                MessageBox.Show("resul is = " + multy);
+            }
+            else
+            {
+                MessageBox.Show("Please enter the required values !");
+            }
+        }
+
+        private void Division_Click(object sender, EventArgs e)
+        {
+           
+            if (textBox1.Text != "" && textBox2.Text != "")
+            {
+                double numDivision1 = Convert.ToDouble(textBox1.Text);
+                double numDivision2 = Convert.ToDouble(textBox2.Text);
+                double Division = numDivision1 / numDivision2;
+                MessageBox.Show("resul is = " + Division);
+            }
+            else
+            {
+                MessageBox.Show("Please enter the required values !");
+            }
+        }
+
+        private void modulus_Click(object sender, EventArgs e)
+        {
+           
+
+            if (textBox1.Text != "")
+            {
+                double digit = double.Parse(textBox1.Text);
+                double endmodulus = Math.Abs(digit);
+                MessageBox.Show("resul is = " + endmodulus);
+            }
+            else
+            {
+                MessageBox.Show("Please enter the required amount !");
+            }
+        }
+
+        private void power2_Click(object sender, EventArgs e)
+        {
+           
+            if (textBox1.Text != "")
+            {
+                double power2 = double.Parse(textBox1.Text);
+                double endpower2 = Math.Pow(power2, 2);
+                MessageBox.Show("resul is = " + endpower2);
+            }
+            else
+            {
+                MessageBox.Show("Please enter the required amount !");
+            }
+        }
+
+        private void radical_Click(object sender, EventArgs e)
+        {
+           
+            if (textBox1.Text != "")
+            {
+                double radical = double.Parse(textBox1.Text);
+                double endradical = Math.Sqrt(radical);
+                MessageBox.Show("resul is = " + endradical);
+            }
+            else
+            {
+                MessageBox.Show("Please enter the required amount !");
+            }
+        }
+        private void factorial_Click(object sender, EventArgs e)
+        {
+           
+            if (textBox1.Text != "")
+            {
+                double factorial = 1, digitfactorial;
+                digitfactorial = double.Parse(textBox1.Text);
+                for (int i = 1; i <= digitfactorial; i++)
+                {
+                    factorial *= i;
+                }
+                MessageBox.Show("resul is : " + factorial);
+            }
+            else
+            {
+                MessageBox.Show("Please enter the required amount !");
+            }
+        }
+
+        private void power_Click(object sender, EventArgs e)
+        {
+           
+            if (textBox1.Text != "" && textBox2.Text != "")
+            {
+                double power = double.Parse(textBox1.Text);
+                double powerr = double.Parse(textBox2.Text);
+                double endpower = Math.Pow(power, powerr);
+                MessageBox.Show("resul is = " + endpower);
+            }
+            else
+            {
+                MessageBox.Show("Please enter the required values !");
+            }
+        }
+
+        private void logarithm_Click(object sender, EventArgs e)
+        {
+          
+            if (textBox1.Text != "")
+            {
+                double digitlogarithm = double.Parse(textBox1.Text);
+                double resullogarithm = Math.Log(digitlogarithm, 10);
+                MessageBox.Show($"The logarithm of the number {digitlogarithm} is equal to {resullogarithm}");
+            }
+            else
+            {
+                MessageBox.Show("Please enter the required amount !");
+            }
+        }
+
+        private void lnlog_Click(object sender, EventArgs e)
+        {
+
+          
+            if (textBox1.Text != "")
+            {
+                double digitlnlog = double.Parse(textBox1.Text);
+                double resullnlog = Math.Log(digitlnlog);
+                MessageBox.Show("resul is : " + resullnlog);
+            }
+            else
+            {
+                MessageBox.Show("Please enter the required amount !");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Note: Text box number one has priority for operations such as logarithms and powers of 2!");
+        }
+    }
+}
